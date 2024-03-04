661906a48fb05958587a2a83b82b1599aef3a929
rjaramilloh <159493247+rjaramilloh@users.noreply.github.com>
2024-03-04 15:35:15 -0500

    Momento 2
    corrección del **kawargs

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,18 +3,20 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas={}, estudiantes=[]):
+    def __init__(self,grupo="grupo predeterminado", asignaturas=[], estudiantes = []):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
+
 
     def agregarAlumno(self, alumno, lista=[]):
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
+        lista.remove(alumno)
 
     # def __str__(self):
     #     pass
